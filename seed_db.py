7bb52e2125762d9f99aa4494123cfd2760bdacd3
Rudeous <mpkennygoh@gmail.com>
2022-09-14 06:59:22 +0800

    added database seeding entries for testing

--- seed_db.py
+++ seed_db.py
@@ -20,6 +20,9 @@ Base.metadata.create_all(bind=engine)
 household1 = Household(household_id='1', housing_type=Housing_Type.landed)
 household2 = Household(household_id='2', housing_type=Housing_Type.condo)
 household3 = Household(household_id='3', housing_type=Housing_Type.hdb)
+household4 = Household(household_id='4', housing_type=Housing_Type.hdb)
+household5 = Household(household_id='5', housing_type=Housing_Type.condo)
+household6 = Household(household_id='6', housing_type=Housing_Type.landed)
 
 # family member population
 family_member1 = Family_Member(
@@ -27,11 +30,10 @@ family_member1 = Family_Member(
     household_id='1', 
     name='John', 
     gender=Gender.male, 
-    marital_status=Marital_Status.married, 
-    spouse_id='2', 
-    occupation_type=Occupation_Type.employed, 
+    marital_status=Marital_Status.single, 
+    occupation_type=Occupation_Type.student, 
     annual_income='3000', 
-    dob= datetime.strptime('1980-01-01', "%Y-%m-%d")
+    dob= datetime.strptime('2011-01-01', "%Y-%m-%d")
     )
 
 family_member2 = Family_Member(
@@ -39,20 +41,97 @@ family_member2 = Family_Member(
     household_id='1', 
     name='Mary', 
     gender=Gender.female, 
-    marital_status=Marital_Status.married, 
-    spouse_id='1', 
-    occupation_type=Occupation_Type.employed,
+    marital_status=Marital_Status.single, 
+    occupation_type=Occupation_Type.student,
     annual_income='3100',
-    dob= datetime.strptime('1985-02-02', "%Y-%m-%d")
+    dob= datetime.strptime('2012-02-02', "%Y-%m-%d")
+    )
+
+family_member3 = Family_Member(
+    family_member_id='3', 
+    household_id='2', 
+    name='Ben', 
+    gender=Gender.male, 
+    marital_status=Marital_Status.single, 
+    occupation_type=Occupation_Type.student,
+    annual_income='0',
+    dob= datetime.strptime('2013-03-03', "%Y-%m-%d")
+    )
+
+family_member4 = Family_Member(
+    family_member_id='4', 
+    household_id='2', 
+    name='Sue', 
+    gender=Gender.female, 
+    marital_status=Marital_Status.single, 
+    occupation_type=Occupation_Type.employed,
+    annual_income='50000',
+    dob= datetime.strptime('1950-04-04', "%Y-%m-%d")
+    )
+
+family_member5 = Family_Member(
+    family_member_id='5', 
+    household_id='3', 
+    name='Tom', 
+    gender=Gender.male, 
+    marital_status=Marital_Status.married, 
+    spouse_id='6', 
+    occupation_type=Occupation_Type.employed,
+    annual_income='1500000',
+    dob= datetime.strptime('1940-05-05', "%Y-%m-%d")
+    )
+
+family_member6 = Family_Member(
+    family_member_id='6', 
+    household_id='3', 
+    name='Sherry', 
+    gender=Gender.female, 
+    marital_status=Marital_Status.married, 
+    spouse_id='5', 
+    occupation_type=Occupation_Type.employed,
+    annual_income='10000',
+    dob= datetime.strptime('1940-06-06', "%Y-%m-%d")
+    )
+
+family_member7 = Family_Member(
+    family_member_id='7', 
+    household_id='4', 
+    name='Harry', 
+    gender=Gender.male, 
+    marital_status=Marital_Status.single, 
+    occupation_type=Occupation_Type.unemployed,
+    annual_income='0',
+    dob= datetime.strptime('2022-07-07', "%Y-%m-%d")
+    )
+
+family_member8 = Family_Member(
+    family_member_id='8', 
+    household_id='4', 
+    name='Salynn', 
+    gender=Gender.female, 
+    marital_status=Marital_Status.single,  
+    occupation_type=Occupation_Type.employed,
+    annual_income='40000',
+    dob= datetime.strptime('1980-08-08', "%Y-%m-%d")
     )
 
 # add to db
 db_session.add(household1)
 db_session.add(household2)
 db_session.add(household3)
+db_session.add(household4)
+db_session.add(household5)
+db_session.add(household6)
 
 db_session.add(family_member1)
 db_session.add(family_member2)
+db_session.add(family_member3)
+db_session.add(family_member4)
+db_session.add(family_member5)
+db_session.add(family_member6)
+db_session.add(family_member7)
+db_session.add(family_member8)
+
 
 # commit to db
 db_session.commit()
